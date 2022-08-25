c67ae508cbe2f5401328789439b3ad265f28f335
ashutosh7i <ashutoshsoni130@gmail.com>
2022-08-25 08:30:26 +0530

    Create using node js.js

---
+++ using node js.js
@@ -0,0 +1,18 @@
+//What is node.js?
+
+//Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine and executes JavaScript code outside a web browser, 
+//which means using node-js we can run javascript code in a terminal without using a browser
+
+//Steps to set up Node.js
+//Install nodejs from official site, install it, setup the path.
+//all node js file must be of .js extention.
+//to test this ,simply open or node terminal,
+//type this code and press enter
+
+console.log("hello");
+
+//you should see a hello in output.
+
+//NOW to run this as a file, save it as .js,
+//open any terminal, type
+//node filename.js press enter, you should see output in terminal.
