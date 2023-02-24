e8923e8c37449d12665b0e309c70803ed16094df
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-02-24 11:12:09 +0530

    using APIs wiht js

---
+++ usingAPIs.js
@@ -0,0 +1,25 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+// fetch('https://api.domainsdb.info/v1/domains/search?domain=simp')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+
+
+// //with data  
+//   const data = { username: 'example' };
+
+// fetch('https://example.com/profile', {
+//   method: 'POST', // or 'PUT'
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(data),
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log('Success:', data);
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
