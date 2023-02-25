330c26c50a757d6a81cd8581044fe51f827c19e0
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-02-26 02:02:15 +0530

    typeConversion.js

---
+++ typeConversion.js
@@ -0,0 +1,21 @@
+let a = "1";
+console.log(a);
+console.log(typeof a);
+
+console.log(parseInt(a));
+console.log(typeof parseInt(a));
+
+a = 1.7;
+
+console.log(parseFloat(a));
+console.log(typeof parseFloat(a));
+
+console.log(a.toString());
+console.log(typeof a.toString());
+
+a = 1;
+b = "2";
+
+console.log(a+b);
+let c = a+b;
+console.log(typeof c);
